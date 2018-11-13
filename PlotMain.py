c39d2e4c0d385627ba0c2a3ef8337a83f89ecd77
Tarthir <tylerbrady34@gmail.com>
2018-11-13 10:26:36 -0700

    Adding Histograms, slight bugs, do not use yet.

--- PlotMain.py
+++ PlotMain.py
@@ -5,7 +5,7 @@ import sys
 import ReadFiles as file_reader
 import DataHolderMethods as methods
 
-
+# TODO see if there are different ASNs for both the one we queried and the one who queries our servers
 # example of how to call: python PlotMain.py C:\Users\tyler\PycharmProjects\PlottingPractice\ client
 
 ######################################################################################################
@@ -39,17 +39,18 @@ plotter = plot.Plotter()
 ######################################################################################################
 # AT THIS POINT YOU CAN CALL/USE WHAT FUNCTIONS LOGIC YOU WANT TO CREATE THE NEEDED GRAPHS
 # TODO take out long names for OS's (take out unneeded info in parens)
-
-# DO OS's
-# Grab The inner dicts
+# TODO package up and do json dumps of these dictionaries so they can be put into peoples own graphs
+#
+# # DO OS's
+# # Grab The inner dicts
 all_ips = (list(special_dict.values()))[0]
 num_of_os = {}
 for ip_dict in all_ips.values():
 
-    if ip_dict["os"] not in num_of_os.keys():
-        num_of_os[ip_dict["os"]] = 1
-    else:
-        num_of_os[ip_dict["os"]] += 1
+     if ip_dict["os"] not in num_of_os.keys():
+         num_of_os[ip_dict["os"]] = 1
+     else:
+         num_of_os[ip_dict["os"]] += 1
 plotter.categorical_bar_graph("Type Of OS's Found", "OS Types", "Number Found", list(num_of_os.keys()), list(num_of_os.values()))
 
 # DO by country
@@ -58,15 +59,15 @@ all_countries = (variable_dict["country"])
 sorted_countries = sorted(all_countries.items(), key=operator.itemgetter(1), reverse=True)
 top_countries = dict(sorted_countries[:15])
 plotter.categorical_bar_graph("Top 15 Countries", "Countries", "Number Found",
-                              list(top_countries.keys()), list(top_countries.values()))
+                               list(top_countries.keys()), list(top_countries.values()))
 
 # Do by country and include 'Other'
 top_countries['Other'] = 0
 for i in range(15, len(sorted_countries)):
-    top_countries['Other'] += sorted_countries[i][1]
+     top_countries['Other'] += sorted_countries[i][1]
 
 plotter.categorical_bar_graph("Top 15 Countries", "Countries", "Number Found",
-                              list(top_countries.keys()), list(top_countries.values()))
+                               list(top_countries.keys()), list(top_countries.values()))
 
 # Do by company name
 all_companies = variable_dict["shortname"]
@@ -74,9 +75,21 @@ all_companies = variable_dict["shortname"]
 sorted_companies = sorted(all_companies.items(), key=operator.itemgetter(1), reverse=True)
 top_companies = dict(sorted_companies[:10])
 plotter.categorical_bar_graph("Top 10 Companies", "Companies", "Number Found",
-                              list(top_companies.keys()), list(top_companies.values()))
+                               list(top_companies.keys()), list(top_companies.values()))
 
 # Do by Country pie chart
 explode = [0] * len(top_countries.keys())
 explode[0] = 0.1
 plotter.pie_chart("Top 15 Countries", list(top_countries.keys()), list(top_countries.values()), explode)
+
+
+# Do a bar graph on mtu
+all_mtu = variable_dict["raw_mtu"]
+sorted_mtus = sorted(all_mtu.items(), key=operator.itemgetter(1), reverse=True)
+top_mtus = dict(sorted_mtus[:15])
+explode = [0] * len(top_mtus.keys())
+explode[0] = 0.1
+plotter.categorical_bar_graph("Top MTU", "a", "a", list(top_mtus.keys()), list(top_mtus.values()))
+# Do Hist for dist
+#dist = variable_dict["dist"]
+#plotter.hist("a", "a", "a", dist, 50)
--- Plotter.py
+++ Plotter.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 from collections import namedtuple
+from numpy import array
 
 
 class Plotter(object):
@@ -25,19 +26,79 @@ class Plotter(object):
         ax.set_title(title)
         plt.xticks(x_pos, cat_x_values)
         plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-
+        # fig.tight_layout()
         plt.show()
 
+    # This method creates pie charts from given data
+    # title: the title you want for the chart
+    # labels: An array where each element is the name of a slice of the pie
+    # sizes: An array numbers, each representing how big each slice will be
+    # explode: An array of zeroes, anything non zero, like 0.1 will cause
+    # the respective slice to be more pronounced
     def pie_chart(self, title, labels, sizes, explode):
         # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-        #labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
-        #sizes = [15, 30, 45, 10]
 
         fig1, ax = plt.subplots()
-        ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-                shadow=True, startangle=90)
+        ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
         ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
         ax.set_title(title)
 
         plt.show()
 
+    def hist(self, title, x_label, y_label, values, num_of_bins):
+        self.__check_dict(values)
+
+        # mean of distribution
+        mu = self.__get_mean(values)
+        # standard deviation of distribution
+        sigma = self.__get_std_dev(values)
+
+        num_bins = num_of_bins
+
+        fig, ax = plt.subplots()
+
+        # the histogram of the data
+        # TODO fix array thingie
+        n, bins, patches = ax.hist(list(values.values()), num_bins, density=1)
+
+        # add a 'best fit' line
+        y = ((1 / (np.sqrt(2 * np.pi) * sigma)) *
+             np.exp(-0.5 * (1 / sigma * (bins - mu)) ** 2))
+        ax.plot(bins, y, '--')
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        ax.set_title(title)
+
+        # Tweak spacing to prevent clipping of ylabel
+        fig.tight_layout()
+        plt.show()
+
+    def __get_mean(self, values):
+        mu = 0
+        num = 0
+        for distance in values.keys():
+            try:
+                mu += int(distance) * values[distance]
+            except ValueError:
+                continue
+            num += values[distance]
+        return mu // num  # mean of distribution
+
+    def __get_std_dev(self, values):
+        arr = [[int(dist)] * int(values[dist]) for dist in values.keys()]
+        new_arr = []
+        for i in range(len(arr)):
+            new_arr.extend(arr[i])
+        new_arr = array(new_arr)
+        std = np.sqrt(np.mean(abs(new_arr - new_arr.mean()) ** 2))
+        return std
+
+    def __check_dict(self, values):
+        to_be_removed = []
+        for key in values.keys():
+            try:
+                int(key)
+            except ValueError:
+                to_be_removed.append(key)
+        for remove in to_be_removed:
+            values.pop(remove, None)
